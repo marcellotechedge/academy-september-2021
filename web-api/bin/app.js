78ba3cf02ff4d5a8e98a088fd30be7bb76ac7586
AzureAD\MarcelloRossi <marcello.rossi@techedgegroup.com>
2021-04-23 15:30:16 +0200

    endpoints

--- web-api/bin/app.js
+++ web-api/bin/app.js
@@ -1,2 +1,15 @@
-console.log("hello typescript");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+app.use(express.json());
+const PORT = 3001;
+app.get("/case", (req, res) => {
+    res.send({
+        message: "hello World"
+    });
+});
+app.listen(PORT, () => {
+    console.log("Running on port", PORT);
+});
 //# sourceMappingURL=app.js.map
--- web-api/src/app.ts
+++ web-api/src/app.ts
@@ -1,1 +1,34 @@
-console.log("hello typescript111");
+import express = require('express');
+var cors = require('cors')
+const app = express();
+app.use(cors())
+app.use(express.json());
+const PORT = 3006;
+
+app.get("/case", (req: any, res: any) => {
+    res.send("xxx");
+});
+
+app.get("/case-summary", (req: any, res: any) => {
+    res.send("xxx");
+});
+
+app.post("/case", (req: any, res: any) => {
+    res.send("xxx");
+});
+
+app.put("/case", (req: any, res: any) => {
+    res.send("xxx");
+});
+
+app.delete("/case", (req: any, res: any) => {
+    res.send("xxx");
+});
+
+app.get("/countries", (req: any, res: any) => {
+    res.send("xxx");
+});
+
+app.listen(PORT, () => {
+    console.log("Running on port", PORT);
+});
