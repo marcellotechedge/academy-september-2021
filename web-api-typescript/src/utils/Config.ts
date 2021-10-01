dd9eb22023e6059a797fa233fbb1195372e62da8
flipomitechedge <83018537+flipomitechedge@users.noreply.github.com>
2021-10-01 19:51:54 +0200

    Update Config.ts

--- web-api-typescript/src/utils/Config.ts
+++ web-api-typescript/src/utils/Config.ts
@@ -1,6 +1,6 @@
 export const APPLICATION_PORT = process.env.PORT || 3006;
-export const HOST = process.env.HOST || "academy-may-2021-db.mysql.database.azure.com";
+export const HOST = process.env.HOST || "techacademy-mysql-server.mysql.database.azure.com";
 export const DB_PORT = (process.env.DB_PORT || 3306) as number;
-export const DB_USERNAME = process.env.DB_USERNAME || "academyadmin@academy-may-2021-db";
-export const PASSWORD = process.env.PASSWORD || "Techedge01!";
-export const DATABASE = process.env.DATABASE || "academy";
+export const DB_USERNAME = process.env.DB_USERNAME || "techadmin@techacademy-mysql-server";
+export const PASSWORD = process.env.PASSWORD || "mysqlpassword_2021";
+export const DATABASE = process.env.DATABASE || "c19_tracker";
